c4a9706e9f65fa4bb59b4c3a72d1058f884ae60a
Amanda V Colle <145784896+amandavitoriacolle@users.noreply.github.com>
2025-10-21 11:14:21 -0300

    johnnie

--- main.js
+++ main.js
@@ -71,4 +71,16 @@ const perguntas = [
             }
         ]
     },
+    
 ];
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
+
+
+mostraPergunta();
