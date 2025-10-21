d40b0c67081128f1ad1f167e7137bd55c04af3ad
Amanda V Colle <145784896+amandavitoriacolle@users.noreply.github.com>
2025-10-21 11:32:45 -0300

    johnnie

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ const perguntas = [
             }
         ]
     },
-    
+
 ];
 
 let atual = 0;
@@ -80,6 +80,19 @@ let perguntaAtual;
 function mostraPergunta(){
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for (const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", function(){
+            atual++
+            mostraPergunta();
+        })
+        caixaAlternativas.appendChild(botaoAlternativas);
+    } 
 }
 
 
